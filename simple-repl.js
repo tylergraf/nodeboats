01f0d521859d67928c2605fab0cbce3b006e2a75
Gabriel Dayley <gabriel@Gabriels-MacBook-Pro.local>
2016-06-01 10:11:24 -0600

    simple repl

---
+++ simple-repl.js
@@ -0,0 +1,34 @@
+var five = require("johnny-five");
+var Particle = require("particle-io");
+
+
+var board = new five.Board({
+  io: new Particle({
+    token: process.env.PARTICLE_TOKEN,
+    deviceId: process.env.PARTICLE_DEVICE_ID
+  })
+});
+
+board.on('error', function(err) {
+  console.log('error')
+});
+
+board.on("ready", function() {
+  console.log("Device Ready..");
+  
+  var motorL = new five.Motor({
+    pin: 'A4'
+  });
+
+  var motorR = new five.Motor({
+    pin: 'A5'
+  });
+
+  var servo = new five.Servo("D0");
+
+  board.repl.inject({
+    l: motorL,
+    r: motorR,
+    s: servo
+  });
+});
