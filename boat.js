da8dda10ec1180eb6f2a67b11a6b77c51ab8e71e
gmdayley <gabriel.dayley@gmail.com>
2017-05-25 14:02:08 -0600

    refactoring

--- boat.js
+++ boat.js
@@ -1,52 +1,51 @@
-var util = require("util");
-var EventEmitter = require("events").EventEmitter;
-var five = require("johnny-five");
-var Particle = require("particle-io");
+const EventEmitter = require("events").EventEmitter;
+const five = require("johnny-five");
+const Particle = require("particle-io");
 
+const DEFAULT_CONFIG = {
+  MOTOR_L_PIN: "A4",
+  MOTOR_R_PIN: "A5",
+  RUDDER_PIN: "D0",
+  RUDDER_START_POS: 90
+};
 
-function Boat() {
-  EventEmitter.call(this);
+class Boat extends EventEmitter {
+  constructor() {
+    const { Motor, Servo } = five;
 
-  var board = new five.Board({
-    io: new Particle({
-      token: process.env.PARTICLE_TOKEN,
-      deviceId: process.env.PARTICLE_DEVICE_ID
-    })
-  });
-  
-  
-  board.on("ready", function () {
-    console.log("Device Ready..");
+    this.board = new five.Board({
+      io: new Particle({
+        token: process.env.PARTICLE_TOKEN,
+        deviceId: process.env.PARTICLE_DEVICE_ID
+      })
+    });
 
-    this.motorL = new five.Motor({ pin: 'A4'});
-    this.motorR = new five.Motor({ pin: 'A5'});
-    this.rudder = new five.Servo("D0");
+    board.on("ready", () => {
+      this.motorL = new Motor({ pin: DEFAULT_CONFIG.MOTOR_L_PIN });
+      this.motorR = new Motor({ pin: DEFAULT_CONFIG.MOTOR_R_PIN });
+      this.rudder = new Servo(DEFAULT_CONFIG.RUDDER_PIN);
+      this.rudder.to(DEFAULT_CONFIG.RUDDER_START_POS);
+      this.emit("ready");
+    });
+  }
 
-    this.rudder.to(90);
-    
-    this.emit('ready');
+  forward(speed) {
+    this.motorL.start(speed); //255 max?
+    this.motorR.start(speed);
+  }
 
-  }.bind(this));
+  reverse(speed) {}
 
-  this.board = board;
+  turn(degree) {
+    this.rudder.to(degree);
+  }
+
+  stop() {
+    this.motorL.stop();
+    this.motorR.stop();
+  }
+
+  fire() {}
 }
-util.inherits(Boat, EventEmitter);
-
-
-Boat.prototype.forward = function(speed) {
-  this.motorL.start(speed);
-  this.motorR.start(speed);
-};
-
-Boat.prototype.stop = function(speed) {
-  this.motorL.stop();
-  this.motorR.stop();
-};
-
-Boat.prototype.turn = function(degree) {
-  this.rudder.to(degree);
-};
-
-
 
 module.exports = Boat;
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-var Boat = require('./boat');
+var Boat = require("./boat");
 var boat = new Boat();
 
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
 server.listen(8080);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get("/", function(req, res) {
+  res.sendfile(__dirname + "/index.html");
 });
 
-app.get('/gamepad', function (req, res) {
-  res.sendfile(__dirname + '/gamepad.html');
+app.get("/gamepad", function(req, res) {
+  res.sendfile(__dirname + "/gamepad.html");
 });
 
+io.on("connection", function(socket) {
+  socket.emit("hello", { hello: "world" });
 
-io.on('connection', function (socket) {
-  socket.emit('hello', { hello: 'world' });
-  
-  socket.on('data', function(data) {
-    console.log('data', data);
+  socket.on("data", function(data) {
+    console.log("data", data);
     boat.forward(data.speed || 0);
     boat.turn(data.degree || 90);
   });
